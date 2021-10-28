da793ffdff3bc8e795ee5ae0dd0a0025b8b3b4b1
dladncks1217 <dlaxodud1217@gmail.com>
2021-10-28 15:31:55 +0900

    Add: add antd grid code

--- front/components/Applayout.js
+++ front/components/Applayout.js
@@ -1,7 +1,14 @@
 import React from "react";
 import Link from "next/link";
 import PropTypes from "prop-types";
-import { Menu, Input, Button } from "antd";
+import { Menu, Input, Button, Row, Col, Card, Avatar } from "antd";
+
+const dummy = {
+  nickname: "임우찬",
+  Post: [],
+  Followings: [],
+  Followers: [],
+};
 
 const AppLayout = ({ children }) => {
   return (
@@ -26,11 +33,46 @@ const AppLayout = ({ children }) => {
           <Button>회원가입</Button>
         </a>
       </Link>
+      <Row>
+        <Col xs={24} md={6}>
+          <Card
+            actions={[
+              <div key="twit">
+                짹짹
+                <br />
+                {dummy.Post.length}
+              </div>,
+              <div key="following">
+                짹짹
+                <br />
+                {dummy.Followings.length}
+              </div>,
+              <div key="follower">
+                짹짹
+                <br />
+                {dummy.Followers.length}
+              </div>,
+            ]}
+          >
+            <Card.Meta
+              avatar={<Avatar>{dummy.nickname[0]}</Avatar>}
+              title={dummy.nickname}
+            />
+          </Card>
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          세번째
+        </Col>
+      </Row>
       {children}
     </div>
   );
 };
-
+// antd는 기본적으로 반응형. (한 줄이 24임.)
+// xs는 모바일, md는 데스크탑
 AppLayout.propTypes = {
   // ts하는게 아니라 prop-types로 검증해주면 좋을듯.
   Component: PropTypes.node,
