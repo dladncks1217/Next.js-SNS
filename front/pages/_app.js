4e6a9579ef9c5b644dce5bf6ac4c788536b0548e
dladncks1217 <dlaxodud1217@gmail.com>
2021-11-03 22:30:03 +0900

    Add: connect redux devtools codes

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -3,11 +3,11 @@ import Head from "next/head";
 import PropTypes from "prop-types";
 import withRedux from "next-redux-wrapper";
 import AppLayout from "../components/Applayout";
-import { createStore } from "redux";
+import { applyMiddleware, createStore, compose } from "redux";
 import { Provider } from "react-redux"; // 이 provider는 컴포넌트임.
 import reducer from "../reducers";
 
-const NodeBird = ({ Component, store }) => {
+const NodeBird = ({ Component }) => {
   // nodebird에 store를 넣어줄 부분이 없어서 이걸 next-redux-wrapper를 사용하는 것이다.
   return (
     <>
@@ -33,8 +33,18 @@ NodeBird.propTypes = {
 
 export default withRedux((initialState, options) => {
   // reducer action이 dispatch될 때 state를 어떻게 정의해야할지 정해둔게 reducer였음 여기서 state와 reducer가 합쳐진게 아래 store라고 보면 됨.
-  const store = createStore(reducer, initialState);
+  const middlewares = []; // 이부분만 계속 바뀐다고 보면 될듯.
+  const enhancer = compose(
+    // 액션과 리듀서 사이라 미들웨어
+    applyMiddleware(...middlewares),
+    typeof window !== "undefined" &&
+      window.__REDUX_DEVTOOLS_EXTENSION__ !== "undefined" // redux devtools 사이트에서 가져옴
+      ? window.__REDUX_DEVTOOLS_EXTENSION__() // redux devtools 깔면 window객체에 __REDUX... 하는 함수 생김( 그거 쓰는거임 )
+      : (f) => f // 기본 미들웨어에 __REDUX__DEVTOOLS~~하는거 추가로 해서 합성하겠다는 의미(그냥 외우자.)
+  ); // 미들웨어들 합성 가능. 기능 추가 그런느낌이라 enhancer
+  const store = createStore(reducer, initialState, enhancer);
   // 여기에 store 커스터마이징
+
   return store;
 })(NodeBird); // 이런걸 고차 컴포넌트라고 부름. 컴포넌트의 기능 확장.
 
